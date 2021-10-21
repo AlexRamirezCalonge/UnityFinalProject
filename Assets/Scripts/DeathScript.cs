896233adca37b76d56f92baa029c5a689d1b4631
AlexRamirezCalonge <alex.ramirez.calonge@gmail.com>
2021-10-21 17:04:19 +0200

    updated death trigger and respawn

---
+++ Assets/Scripts/DeathScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    private GameManager m_GameManager = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_GameManager = FindObjectOfType<GameManager>();
+        //m_GameManager=get
+    }
+    /// <summary>
+    /// Some collider have enter on the death trigger
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            m_GameManager.RespawnPlayer();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
